12eb734b15f37c572061eae826a2575af48355cc
Nilton Dias <ntjdias@gmail.com>
2020-10-14 16:30:50 -0300

    Frontend instalado

--- todo-app/backend/src/config/cors.js
+++ todo-app/backend/src/config/cors.js
@@ -4,5 +4,6 @@ module.exports = function(req, res, next) {
     res.header('Access-Control-Allow-Origin','*')
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
     res.header('Access-Control-Allow-Headrs','Origin, X-Requested-With, Content-Type, Accept')    
+    next()
 }
 
